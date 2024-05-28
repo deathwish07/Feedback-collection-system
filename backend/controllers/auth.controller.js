05149316385ba48709a78f9aba3800c01fffb1a5
deathwish07 <ayanaratharva@gmail.com>
2024-05-28 18:56:20 +0530

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async(req,res) => {
+    try {
+        const {firstName, lastName, }= req.body;
+    } catch (error) {
+        
+    }
+};
+
+export const login = (req,res) => {
+    console.log("loginadmin")
+};
+
+export const logout = (req,res) => {
+    console.log("logout")
+};
